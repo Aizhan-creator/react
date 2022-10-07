55622f4c3422b378e0deb78367d874350681a16f
Aizhan Maratkyzy <73554654+Aizhan-creator@users.noreply.github.com>
2022-10-08 00:00:38 +0600

    Update lab4.js

--- lab4.js
+++ lab4.js
@@ -1,1 +1,52 @@
+// Создайте класс Book с параметрами: 
 
+class Book{
+  
+   #name;
+   #code;
+   #pages;
+  
+
+   constructor(name, code, pages) {
+      this.setName(name);
+      this.setCode(code);
+      this.setPages(pages);
+   }
+   getBookData() {
+      return this.name + this.code + this.pages;
+   }
+}
+class ScientificBook extends Book{
+   #price;
+   #publisher;
+
+   constructor(name, code, pages, price, publisher){
+      super(name, code, pages);
+      this.setPrice(price);
+      this.setPublisher(publisher);
+   }
+
+   ScientificBook(name, code, pages, price, publisher){
+      console.log(name);
+   }
+}
+
+const book = new Book("Sarah", "3030", 322);
+console.log(book);
+
+class Library{
+  
+   #name;
+   #city;
+   #country;
+  
+
+   constructor(name, city, country) {
+      this.setName(name);
+      this.setCity(city);
+      this.setCountry(country);
+   }
+   addBook(book) {
+      return this.setName;
+   }
+}
